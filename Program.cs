456ec9ee012b3782be5b53b28bd47fbde124b478
Pinkesh1996 <149655028+Pinkesh1996@users.noreply.github.com>
2024-06-28 10:46:15 +0530

    Change in Program.cs

--- Program.cs
+++ Program.cs
@@ -15,6 +15,25 @@ builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins",    
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:4200",
+           "https://localhost:4200").AllowAnyMethod().AllowAnyHeader().AllowCredentials().SetIsOriginAllowed((hosts) => true);
+    }));
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -25,7 +44,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("NgOrigins");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
